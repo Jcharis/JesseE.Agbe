926e35561c086964c4443a164b3701900d7add8e
JCharis Jesse <jcharistech@gmail.com>
2018-11-04 17:11:11 +0200

    Add Portfolio Site

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    // TABS INIT
+     $('.tabs').tabs();
+      // CAROUSEL INIT
+      $('.carousel.carousel-slider').carousel({ fullWidth: true });
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
