b6dafa2772ba0c16bcc32c34e67d16198620561c
JCharis Jesse <jcharistech@gmail.com>
2018-11-17 13:54:52 +0200

    Updated Init with Sliders

--- js/init.js
+++ js/init.js
@@ -7,6 +7,15 @@
      $('.tabs').tabs();
       // CAROUSEL INIT
       $('.carousel.carousel-slider').carousel({ fullWidth: true });
+         // SLIDER INIT
+      $('.slider').slider({
+        indicators: false,
+        // we don't want the little dots to show
+        height: 500,
+        transition: 500,
+        interval: 6000
+        // how long the slide stays for
+      });
 
 
   }); // end of document ready
